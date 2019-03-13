4fbbefa5087b522541a7a7b2b8c801d30989fef4
Restu <Restu.Santosa@mitrais.com>
2019-03-13 16:25:58 +0800

    insert function not yet done

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+const apiUrl = "/api/employees";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +16,12 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers() {
+  getEmployee() {
     return this.http.get('http://localhost:2020/api/employees')
   }
+
+  postEmployee(data): Observable<any>{
+    return this.http.post(apiUrl, data, httpOptions);
+  }
+
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
+// import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+
 
 
 @Component({
@@ -8,16 +11,30 @@ import { DataService } from '../data.service';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
+
+  // insertForm: FormGroup;
+  empName: string = '';
+  empAddress: string = '';
+  empPhone: number = 0;
+  success = false;
+  submited = false;
   
   users: Object;
-  constructor(private data: DataService) { }
+
+  constructor(private data: DataService, private router: Router) { }
 
   ngOnInit() {
-    this.data.getUsers().subscribe(data => {
+    this.data.getEmployee().subscribe(data => {
         this.users = data
         console.log(data);
       }
     );
   }
+
+  // onFormSubmit(form:NgForm){
+  //   this.data.postEmployee(form).subscribe(res => {
+  //     let id = res['_id'];
+  //   })
+  // }
 
 }
